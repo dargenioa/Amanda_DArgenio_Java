dcc882ee7bc76f208816bba0919e5ccffc9b77d6
Amanda D'Argenio <dargenioa14@gmail.com>
2021-06-07 21:32:39 -0400

    tested method to see it generates a random index for each quote, used Swagger to generate yaml for documents,  added to root of project

--- M2-Challenge-DArgenio-Amanda/random-quote-generator/src/main/java/com/company/randomquotegenerator/controller/QuoteController.java
+++ M2-Challenge-DArgenio-Amanda/random-quote-generator/src/main/java/com/company/randomquotegenerator/controller/QuoteController.java
@@ -17,14 +17,14 @@ public class QuoteController {
 
     private static List<Quote> quoteList = new ArrayList<>(Arrays.asList(
             new Quote(idCounter++, "Nelson Mandela", "The greatest glory in living lies not in never falling, but in rising every time we fall."),
-            new Quote(idCounter++, "Walt Disney","The way to get started is to quit talking and begin doing."),
-            new Quote(idCounter++, "Robert Louis Stevenson"," Don't judge each day by the harvest you reap but by the seeds that you plant."),
+            new Quote(idCounter++, "Walt Disney", "The way to get started is to quit talking and begin doing."),
+            new Quote(idCounter++, "Robert Louis Stevenson", " Don't judge each day by the harvest you reap but by the seeds that you plant."),
             new Quote(idCounter++, "Benjamin Franklin", "Tell me and I forget. Teach me and I remember. Involve me and I learn."),
-            new Quote(idCounter++,"Babe Ruth", "Never let the fear of striking out keep you from playing the game." ),
-            new  Quote(idCounter++, "Steve Jobs", "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma -- which is living with the results of other people's thinking."),
+            new Quote(idCounter++, "Babe Ruth", "Never let the fear of striking out keep you from playing the game."),
+            new Quote(idCounter++, "Steve Jobs", "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma -- which is living with the results of other people's thinking."),
             new Quote(idCounter++, "Ayn Rand", "The question isn't who is going to let me; it's who is going to stop me."),
             new Quote(idCounter++, "Michel De Montaigne", "He who fears he will suffer, already suffers because he fears."),
-            new Quote(idCounter++, "Thich Nhat Hanh","The most precious gift we can offer others is our presence. When mindfulness embraces those we love, they will bloom like flowers."),
+            new Quote(idCounter++, "Thich Nhat Hanh", "The most precious gift we can offer others is our presence. When mindfulness embraces those we love, they will bloom like flowers."),
             new Quote(idCounter++, "Grace Hopper", "The most damaging phrase is, \"It's always been done this way\".")
     ));
 
@@ -34,12 +34,20 @@ public class QuoteController {
 
         int minIndex = 1;
         int maxIndex = quoteList.size();
-        int randomIndex = (int)Math.floor(Math.random() * (maxIndex - minIndex + 1));
+        int randomIndex = (int) Math.floor(Math.random() * (maxIndex - minIndex + 1));
+        int index;
 
         Quote result = quoteList.get(randomIndex);
         System.out.println(result);
         return result;
 
+    }
+
+    @GetMapping(value = "/all")
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<Quote> getAll() {
+
+        return quoteList;
     }
 
 }
---
+++ M2-Challenge-DArgenio-Amanda/random-quote-generator/src/test/java/com/company/randomquotegenerator/controller/QuoteControllerTest.java
@@ -0,0 +1,30 @@
+package com.company.randomquotegenerator.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(QuoteController.class)
+public class QuoteControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnRandomQuoteWithAnIdBetween1And10() throws Exception {
+
+        mockMvc.perform(get("/quote"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty());
+    }
+}

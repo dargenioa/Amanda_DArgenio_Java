eae7f3477b38552602ed96c11c8b5d63a23d99dc
Amanda D'Argenio <dargenioa14@gmail.com>
2021-06-07 20:08:07 -0400

    initial commit for the random quote generator project

---
+++ M2-Challenge-DArgenio-Amanda/random-quote-generator/src/main/java/com/company/randomquotegenerator/RandomQuoteGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.company.randomquotegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomQuoteGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomQuoteGeneratorApplication.class, args);
+	}
+
+}
---
+++ M2-Challenge-DArgenio-Amanda/random-quote-generator/src/test/java/com/company/randomquotegenerator/RandomQuoteGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.randomquotegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomQuoteGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
